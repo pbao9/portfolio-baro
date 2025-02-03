f5168ee2eec06164218ae9eb9bfdfc02a3bf4084
pbao9 <pbao.business@gmail.com>
2025-02-03 20:25:01 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className="flex justify-center bg-black text-white">
+                <h1>Hello world</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
