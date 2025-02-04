fd8874671e2859fc3586ea7c69fcd08dcc2f512a
pbao9 <pbao.business@gmail.com>
2025-02-05 01:14:48 +0700

    update

--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -1,5 +1,20 @@
+import ButtonShadow from '../input/Button'
+
 const Footer = () => {
-    return <div className='py-5'>Made by <span className='text-pink-dark'>Baro Pham</span> </div>
+    return (
+        <>
+            <div className="py-5">
+                Made by <span className="text-pink-dark">Baro Pham</span>{' '}
+            </div>
+
+            <div className="fixed bottom-5 right-5 z-[999]">
+                <ButtonShadow
+                    path="https://www.facebook.com/"
+                    title="Want this template? Contact now!"
+                />
+            </div>
+        </>
+    )
 }
 
 export default Footer
