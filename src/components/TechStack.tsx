3cf31025b1c585a5ce5a202d8c8b6f90c7e1ec2a
pbao9 <pbao.business@gmail.com>
2025-02-04 22:20:41 +0700

    update responsive slider

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -12,7 +12,33 @@ function TechStack() {
         autoplay: true,
         speed: 2000,
         autoplaySpeed: 2000,
-        cssEase: "linear"
+        cssEase: "linear",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     }
     return (
         <div className="slider-container">
