6ca5e8fc0b92eaff3e8d037167630dc42095fd47
pbao9 <pbao.business@gmail.com>
2025-02-07 00:11:47 +0700

    update screen home

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,6 +1,6 @@
-import {Helmet} from "react-helmet-async";
-import ShinyText from "../blocks/TextAnimations/ShinyText/ShinyText.tsx";
-import {CardCustom} from "../components/CardCustom.tsx";
+import { Helmet } from 'react-helmet-async'
+import ShinyText from '../blocks/TextAnimations/ShinyText/ShinyText.tsx'
+import { CardCustom } from '../components/CardCustom.tsx'
 
 export const Experience = () => {
     return (
@@ -8,25 +8,29 @@ export const Experience = () => {
             <Helmet>
                 <title>Baro | Experience</title>
             </Helmet>
-            <div className='w-full animate-fade-down'>
+            <div className="w-full animate-fade-down">
                 <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl my-5">
-                    Over 1 Years of<br/> Develop
+                    Over 1 Years of
+                    <br /> Develop
                     <ShinyText
                         text="Expertise"
                         disabled={false}
                         speed={3}
                         className="custom-class mx-2"
-
                     />
                 </h1>
             </div>
 
             <div>
-                <CardCustom title={'Mevivu Technology'}
-                            desc={'Developed scalable websites and APIs with Laravel, built custom WordPress e-commerce sites, and managed web hosting with DirectAdmin.' +
-                                ' Worked closely with frontend developers and business analysts to enhance user experience and project efficiency.'}
-                            link={'/experience/mevivu'} date={'23/10/2023 - 26/11/2024'}/>
-
+                <CardCustom
+                    title={'Mevivu Technology'}
+                    desc={
+                        'Developed scalable websites and APIs with Laravel, built custom WordPress e-commerce sites, and managed web hosting with DirectAdmin.' +
+                        ' Worked closely with frontend developers and business analysts to enhance user experience and project efficiency.'
+                    }
+                    link={'/experience/mevivu'}
+                    date={'23/10/2023 - 26/11/2024'}
+                />
             </div>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
 import { Helmet } from 'react-helmet-async'
 import Hero from '../components/Hero'
-import TechStack from "../components/TechStack.tsx";
+import TechStack from '../components/TechStack.tsx'
+import { Experience } from './Experience.tsx'
+import Projects from './Projects.tsx'
 const Home = () => {
     return (
         <div>
+            <Hero />
+            <Projects />
+            <Experience />
+            <TechStack />
             <Helmet>
                 <title>Baro</title>
             </Helmet>
-            <Hero />
-            <TechStack/>
         </div>
     )
 }
