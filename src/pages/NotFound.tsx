344013c9167f5683d98718f575056ad72a403e38
pbao9 <pbao.business@gmail.com>
2025-02-13 23:04:43 +0700

    update

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center h-screen text-center bg-black opacity-90 text-white">
-            <h1 className="text-6xl font-bold glitch-text relative mb-4">
+            <h1 className="text-6xl font-bold animate-glitch glitch-text lative mb-4">
                 404
             </h1>
             <p className="text-xl text-gray-400 mb-4">Oops! Page not found.</p>
